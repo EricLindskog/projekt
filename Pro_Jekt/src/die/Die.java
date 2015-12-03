69759b4d892c4a49df6f8540bcaba9ea89a9b9c6
Filin764 <ingvarsson.filip@gmail.com>
2015-12-03 10:14:49 +0100

    roll shit

--- Pro_Jekt/src/die/Die.java
+++ Pro_Jekt/src/die/Die.java
@@ -1,10 +1,13 @@
 package die;
 
+import java.util.Random;
+
 public class Die {
 	private int value;
-	
+	Random roll = new Random();
 	public void roll(){
 		
+		setValue(roll.nextInt(5)+1);
 	}
 
 	private int getValue() {
