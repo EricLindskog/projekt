267089ad683d91c556027094eab920e6c95554e8
Eric Lindskog <erili798@li12-10.ida.liu.se>
2015-12-03 10:30:36 +0100

    runner

--- Pro_Jekt/src/main/runner.java
+++ Pro_Jekt/src/main/runner.java
@@ -1,10 +1,29 @@
 package main;
 
+import java.util.Scanner;
+
 public class runner {
-	public static void Main (String[] args){
+	public static void main (String[] args){
 		System.out.println("Hello world");
-		//tjatja
-		//hej hej
-		//Nהההה
+		Scanner s = new Scanner(System.in);
+		Yatzy game = new Yatzy();
+		int players= -1;
+		int cpu = -1;
+		do{
+			System.out.println("Skriv in antal spelare");
+			if(s.hasNextInt()){
+				players = s.nextInt();
+			}
+			else{s.nextLine();}
+		}while(players==-1);
+
+		do{
+			System.out.println("Skriv in antal datorer");
+			if(s.hasNextInt()){
+				cpu = s.nextInt();
+			}
+			else{s.nextLine();}
+		}while(cpu==-1);
+		game.run(players, cpu);
 	}
 }
