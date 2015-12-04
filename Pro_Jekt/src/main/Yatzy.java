2bb377918907026d05371580fb833d22c7bd2954
Filin764 <ingvarsson.filip@gmail.com>
2015-12-04 09:09:20 +0100

    jframe and shit

--- Pro_Jekt/src/main/Yatzy.java
+++ Pro_Jekt/src/main/Yatzy.java
@@ -26,21 +26,34 @@ public class Yatzy {
 			
 			
 			
-			JPanel panel = new JPanel();
-			panel.setLayout(new FlowLayout());
+			JPanel panel1 = new JPanel();
+			JPanel panel2 = new JPanel();
+			
 			AbstractButton button = new JButton();
 			
 			frame.setVisible(true);
 			frame.setSize(JFrameX, JFrameY);
 			frame.setResizable(false);
-			frame.add(panel);
+			frame.setLayout(new FlowLayout());
 			
-			panel.setBackground(Color.WHITE);
-			panel.setSize(250, JFrameY);
+			panel1.setLocation(0, 0);
+			panel1.setBackground(Color.WHITE);
+			panel1.setSize(350, 700);
+			panel1.setLayout(new FlowLayout());
+			panel1.setAlignmentX(FlowLayout.RIGHT);
+			
+			panel2.setLocation(350, 0);
+			panel2.setBackground(Color.WHITE);
+			panel2.setSize(350, 700);
+			panel2.setLayout(new FlowLayout());
+			panel2.setAlignmentX(FlowLayout.RIGHT);
 			
 			button.setVisible(true);
-			button.setLocation(10,panel.getWidth()-10);
-			panel.add(button);
+			button.setLocation(50,50);
+			
+			frame.add(panel1);
+			frame.add(panel2);
+			panel1.add(button);
 			
 			Player p1 = new Player();
 			p1.printRemaningComb();
